8b4a71b6bfd3907efeef979d80ca66e098d4c9ff
Specter\Petey <markpmainardi@gmail.com>
2021-02-24 18:43:19 -0600

    Add blinking opacity effect while invulnerable

--- Assets/Game/Player/HealthTracker.cs
+++ Assets/Game/Player/HealthTracker.cs
@@ -28,15 +28,18 @@ public class HealthTracker : MonoBehaviour
 
     public IntReactiveProperty Health = new IntReactiveProperty(5);
     public int MaxHealth { get; private set; }
+    public bool DieAtZeroHealth = true; // TODO: Might need to replace with a DeathHandler for animations, etc.
 
     [SerializeField]
-    private float invulnerabilityLength = 2;
+    private float InvulnerabilityLength = 2;
     private float invulnerabilityTimer = 0;
-
     public BoolReactiveProperty IsInvulnerable = new BoolReactiveProperty();
-
-    public bool DieAtZeroHealth = true; // TODO: Might need to replace with a DeathHandler for animations, etc.
     
+    public MeshRenderer Mesh;
+    [SerializeField] private float MinOpacity = 0.35f;
+    [SerializeField] private float CycleOffset = 8f;
+    private float blinkTimer = 0;
+
     public Collider Collider { get; private set; }
 	// ============================================================================================
 
@@ -59,13 +62,27 @@ public class HealthTracker : MonoBehaviour
                 .Where((int h) => h == 0)
                 .Subscribe((int h) => GameObject.Destroy(this.gameObject))
                 .AddTo(this);
+
+        this.IsInvulnerable.Subscribe((bool b) =>
+        {
+            if (b)
+                this.blinkTimer = 0;
+            else
+                this.ResetOpacity();
+
+        }).AddTo(this);
     }
 	// ----------------------------------------------------------------------------------
 	void Update ()
 	{
+        this.blinkTimer += Time.deltaTime;
         this.invulnerabilityTimer -= Time.deltaTime;
-        if (this.IsInvulnerable.Value && this.invulnerabilityTimer <= 0)
-            this.IsInvulnerable.Value = false;
+        if (this.IsInvulnerable.Value)
+        {
+            if (this.Mesh != null)
+                this.BlinkOpacity();
+            this.IsInvulnerable.Value = this.invulnerabilityTimer > 0;
+        }
 	}
     // ----------------------------------------------------------------------------------
     private void OnCollisionEnter(Collision collision)
@@ -86,14 +103,31 @@ public class HealthTracker : MonoBehaviour
             && !this.IsInvulnerable.Value)
         {
             this.Health.Value = Math.Max(this.Health.Value - 1, 0);
-            this.invulnerabilityTimer = this.invulnerabilityLength;
+            this.invulnerabilityTimer = this.InvulnerabilityLength;
             this.IsInvulnerable.Value = true;
+            //this.blinkTimer = 0;
         }
         else if ((this.HealingLayers.value & (1 << other.layer)) != 0)
         {
             this.Health.Value = Math.Min(this.Health.Value + 1, this.MaxHealth);
             GameObject.Destroy(other);
         }
+    }
+
+    private void BlinkOpacity()
+    {
+        Color c = this.Mesh.material.color;
+        c.a = Math.Max(
+            this.MinOpacity,
+            0.5f + 0.5f * Mathf.Cos(2 * (float)Math.Pow(this.blinkTimer + this.CycleOffset, 2))
+            );
+        this.Mesh.material.color = c;
+    }
+    private void ResetOpacity()
+    {
+        Color c = this.Mesh.material.color;
+        c.a = 1;
+        this.Mesh.material.color = c;
     }
 
 }
